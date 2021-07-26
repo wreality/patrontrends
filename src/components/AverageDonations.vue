cc5d4819f74243d4bcc89a6744dfd79df55102c6
Brian Adams <wreality@gmail.com>
2021-07-26 19:07:41 -0400

    Fix broken build

--- src/components/AverageDonations.vue
+++ src/components/AverageDonations.vue
@@ -4,7 +4,7 @@ line-chart(:chartData="data", :options="options", v-if="data")
 
 <script>
 import { LineChart } from "vue-chart-3"
-import { defineComponent, ref, onMounted } from "vue"
+import { defineComponent } from "vue"
 import { DateTime } from "luxon"
 import { Chart, registerables } from "chart.js"
 import annotationPlugin from "chartjs-plugin-annotation"
@@ -23,74 +23,68 @@ export default defineComponent({
     },
   },
   setup(props) {
-    //loop through donors and build a map of date and totals
-    const data = ref({})
-
-    onMounted(async () => {
-      const startEvent = Object.entries(props.project.timeline).find(
-        // eslint-disable-next-line
+    const startEvent = Object.entries(props.project.timeline).find(
+      // eslint-disable-next-line
         ([_, v]) => {
-          return v.type === "event" && v.subtype == "campaign_started"
-        }
-      )[1]
-      const startDate = DateTime.fromISO(startEvent.date).startOf("day")
-      const endDate = DateTime.fromISO(props.project.EndDate).endOf("day")
-      const campaignLength = endDate.diff(startDate, "days").toObject().days
-      const goal = parseFloat(props.project.Goal)
-      //const pledged = parseFloat(props.project.PledgeAmount)
-
-      let acc = 0
-      const dayMap = new Map()
-      dayMap.set(0, 0)
-      Array.from(props.project.donors)
-        .sort((a, b) => DateTime.fromISO(a.Date) - DateTime.fromISO(b.Date))
-        .forEach((donor) => {
-          acc += parseFloat(donor.Amount)
-          const campaignDay = DateTime.fromISO(donor.Date)
-            .startOf("day")
-            .diff(startDate, "days")
-            .toObject().days
-          dayMap.set(campaignDay, acc)
-        })
-      const dayTotals = Array.from(dayMap).reverse()
-
-      const today = DateTime.now().startOf("day")
-
-      const currentDay =
-        today > endDate
-          ? campaignLength
-          : today.diff(startDate, "days").toObject().days
-
-      const averageDonationPerDay = []
-      const remainingDonationNeed = []
-      for (var i = 0; i <= currentDay; i++) {
-        const date = startDate.plus({ days: i })
-        // eslint-disable-next-line
-      const total = dayTotals.find(([day, _]) => day <= i)[1]
-        averageDonationPerDay.push({ x: date, y: total / (i + 1) })
-        if (goal - total > 0) {
-          remainingDonationNeed.push({
-            x: date,
-            y: (goal - total) / (campaignLength - i),
-          })
-        }
+        return v.type === "event" && v.subtype == "campaign_started"
       }
-      console.log(averageDonationPerDay)
-      const datasets = [
-        {
-          label: "Received",
-          data: averageDonationPerDay,
-          radius: 3,
-        },
-        {
-          label: "Needed",
-          data: remainingDonationNeed,
-          radius: 3,
-        },
-      ]
+    )[1]
+    const startDate = DateTime.fromISO(startEvent.date).startOf("day")
+    const endDate = DateTime.fromISO(props.project.EndDate).endOf("day")
+    const campaignLength = endDate.diff(startDate, "days").toObject().days
+    const goal = parseFloat(props.project.Goal)
+    //const pledged = parseFloat(props.project.PledgeAmount)
 
-      data.value = { datasets }
-    })
+    let acc = 0
+    const dayMap = new Map()
+    dayMap.set(0, 0)
+    Array.from(props.project.donors)
+      .sort((a, b) => DateTime.fromISO(a.Date) - DateTime.fromISO(b.Date))
+      .forEach((donor) => {
+        acc += parseFloat(donor.Amount)
+        const campaignDay = DateTime.fromISO(donor.Date)
+          .startOf("day")
+          .diff(startDate, "days")
+          .toObject().days
+        dayMap.set(campaignDay, acc)
+      })
+    const dayTotals = Array.from(dayMap).reverse()
+
+    const today = DateTime.now().startOf("day")
+
+    const currentDay =
+      today > endDate
+        ? campaignLength
+        : today.diff(startDate, "days").toObject().days
+
+    const averageDonationPerDay = []
+    const remainingDonationNeed = []
+    for (var i = 0; i <= currentDay; i++) {
+      const date = startDate.plus({ days: i })
+      // eslint-disable-next-line
+      const total = dayTotals.find(([day, _]) => day <= i)[1]
+      averageDonationPerDay.push({ x: date, y: total / (i + 1) })
+      if (goal - total > 0) {
+        remainingDonationNeed.push({
+          x: date,
+          y: (goal - total) / (campaignLength - i),
+        })
+      }
+    }
+    const datasets = [
+      {
+        label: "Received",
+        data: averageDonationPerDay,
+        radius: 3,
+      },
+      {
+        label: "Needed",
+        data: remainingDonationNeed,
+        radius: 3,
+      },
+    ]
+
+    const data = { datasets }
     const options = {
       plugins: {
         title: {
--- src/components/DonationsPerDay.vue
+++ src/components/DonationsPerDay.vue
@@ -4,7 +4,7 @@ line-chart(:chartData="data", :options="options")
 
 <script>
 import { LineChart } from "vue-chart-3"
-import { defineComponent, toRaw, ref, onMounted } from "vue"
+import { defineComponent, toRaw } from "vue"
 import { DateTime } from "luxon"
 import { Chart, registerables } from "chart.js"
 import annotationPlugin from "chartjs-plugin-annotation"
@@ -27,114 +27,109 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const data = ref({})
-    const options = ref({})
+    const goal = parseFloat(props.project.Goal)
+    const pledged = parseFloat(props.project.PledgeAmount)
 
-    onMounted(async () => {
-      const goal = parseFloat(props.project.Goal)
-      const pledged = parseFloat(props.project.PledgeAmount)
+    let acc = 0
+    const donors = [...props.project.donors]
 
-      let acc = 0
-      const donors = [...props.project.donors]
-
-      donors.sort((a, b) => DateTime.fromISO(a.Date) - DateTime.fromISO(b.Date))
-      const donations = donors.map((donor) => {
-        acc += parseFloat(donor.Amount)
-        const date = DateTime.fromISO(donor.Date)
-        return {
-          x: date,
-          y: acc,
-          donor,
-        }
-      })
-      data.value = {
-        datasets: [
-          {
-            label: "Donations",
-            data: donations,
-            radius: 3,
-          },
-        ],
-      }
-      options.value = {
-        plugins: {
-          title: {
-            display: true,
-            text: "Donation History",
-          },
-          colorschemes: {
-            scheme: "brewer.DarkTwo3",
-          },
-          autocolors: true,
-          tooltip: {
-            callbacks: {
-              afterBody: function (context) {
-                console.log(context)
-                return `${context[0].raw.donor.Name}: ${currency.format(
-                  parseFloat(context[0].raw.donor.Amount)
-                )}`
-              },
-              label: function (context) {
-                var label = context.dataset.label || ""
-                console.log(context)
-                if (label) {
-                  label += ": "
-                }
-                if (context.parsed.y !== null) {
-                  label += new Intl.NumberFormat("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  }).format(context.parsed.y)
-                }
-                return label
-              },
-            },
-          },
-          annotation: {
-            annotations: {
-              goal: {
-                type: "line",
-                value: goal,
-                scaleID: "y",
-                borderColor: "#3FB781",
-                borderWidth: 2,
-                label: {
-                  backgroundColor: "#3FB781",
-                  content: `GOAL: ${currency.format(goal)}`,
-                  color: "#fff",
-                  enabled: true,
-                },
-              },
-              constant: {
-                type: "line",
-
-                yMin: 0,
-                borderDash: [12, 12],
-                yMax: goal,
-                borderColor: "#F1A102",
-                borderWidth: 2,
-              },
-            },
-          },
-        },
-        responsive: true,
-        aspectRatio: 1.75,
-        scales: {
-          x: {
-            type: "time",
-            time: {
-              unit: "day",
-            },
-            max: toRaw(props.project).EndDate,
-          },
-
-          y: {
-            max: goal > pledged ? goal : pledged,
-            min: 0,
-          },
-        },
+    donors.sort((a, b) => DateTime.fromISO(a.Date) - DateTime.fromISO(b.Date))
+    const donations = donors.map((donor) => {
+      acc += parseFloat(donor.Amount)
+      const date = DateTime.fromISO(donor.Date)
+      return {
+        x: date,
+        y: acc,
+        donor,
       }
     })
+    const data = {
+      datasets: [
+        {
+          label: "Donations",
+          data: donations,
+          radius: 3,
+        },
+      ],
+    }
+    const options = {
+      plugins: {
+        title: {
+          display: true,
+          text: "Donation History",
+        },
+        colorschemes: {
+          scheme: "brewer.DarkTwo3",
+        },
+        autocolors: true,
+        tooltip: {
+          callbacks: {
+            afterBody: function (context) {
+              console.log(context)
+              return `${context[0].raw.donor.Name}: ${currency.format(
+                parseFloat(context[0].raw.donor.Amount)
+              )}`
+            },
+            label: function (context) {
+              var label = context.dataset.label || ""
+              console.log(context)
+              if (label) {
+                label += ": "
+              }
+              if (context.parsed.y !== null) {
+                label += new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(context.parsed.y)
+              }
+              return label
+            },
+          },
+        },
+        annotation: {
+          annotations: {
+            goal: {
+              type: "line",
+              value: goal,
+              scaleID: "y",
+              borderColor: "#3FB781",
+              borderWidth: 2,
+              label: {
+                backgroundColor: "#3FB781",
+                content: `GOAL: ${currency.format(goal)}`,
+                color: "#fff",
+                enabled: true,
+              },
+            },
+            constant: {
+              type: "line",
+
+              yMin: 0,
+              borderDash: [12, 12],
+              yMax: goal,
+              borderColor: "#F1A102",
+              borderWidth: 2,
+            },
+          },
+        },
+      },
+      responsive: true,
+      aspectRatio: 1.75,
+      scales: {
+        x: {
+          type: "time",
+          time: {
+            unit: "day",
+          },
+          max: toRaw(props.project).EndDate,
+        },
+
+        y: {
+          max: goal > pledged ? goal : pledged,
+          min: 0,
+        },
+      },
+    }
     return { data, options }
   },
 })
